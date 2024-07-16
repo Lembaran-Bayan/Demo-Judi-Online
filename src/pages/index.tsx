21990c95193e07475410c1487a1520b54ca18565
Ahmad Zaki Akmal <87590846+ahmadzakiakmal@users.noreply.github.com>
2024-07-17 06:48:08 +0700

    feat: changed landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,16 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex justify-center items-center font-jakarta">
-      <h1 className="text-[40px]">Slot Gacor</h1>
+    <main className="min-h-screen flex flex-col justify-center items-center font-jakarta gap-10">
+      <h1>Slot Gacor</h1>
+      <form className="flex flex-col gap-5">
+        <label className="flex gap-3">
+          Username
+          <input type="text" />
+        </label>
+
+        <button type="submit">Log In</button>
+      </form>
     </main>
   );
 }
